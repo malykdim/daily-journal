0d3cce66770004897c7ff33cb211072687642323
Albena Kostova <albena.kostov@gmail.com>
2021-06-24 00:19:14 +0200

    updating info

--- Testimonial Box Switcher/script.js
+++ Testimonial Box Switcher/script.js
@@ -6,21 +6,33 @@ const role = document.querySelector('.role')
 
 const testimonials = [
     {
-        name: 'Miyah Myles',
-        position: 'Marketing',
-        photo: 'https://images.unsplash.com/photo-1520529277867-dbf8c5e0b340?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=333&q=80',
+        name: 'Eddie Jacson',
+        position: 'Musician',
+        photo: 'https://images.unsplash.com/photo-1519345182560-3f2917c472ef?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80',
         text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum necessitatibus est, ratione sunt, quos quasi neque dolorem corporis deleniti illum assumenda praesentium tempore eum recusandae id magni cum similique tenetur harum! Cupiditate, architecto expedita!'
     },
     {
-        name: 'Miyah Myles',
-        position: 'Marketing',
+        name: 'Ray McColin',
+        position: 'Developer',
         photo: 'https://images.unsplash.com/photo-1618077360395-f3068be8e001?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80',
         text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum necessitatibus est, ratione sunt, quos quasi neque dolorem corporis deleniti illum assumenda praesentium tempore eum recusandae id magni cum similique tenetur harum! Cupiditate, architecto expedita!'
     },
     {
         name: 'Ruth Morales',
-        position: 'Marketing',
+        position: 'Designer',
         photo: 'https://images.unsplash.com/photo-1520529277867-dbf8c5e0b340?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=333&q=80',
+        text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum necessitatibus est, ratione sunt, quos quasi neque dolorem corporis deleniti illum assumenda praesentium tempore eum recusandae id magni cum similique tenetur harum! Cupiditate, architecto expedita!'
+    },
+    {
+        name: 'Miyah Myles',
+        position: 'Marketing',
+        photo: 'https://images.unsplash.com/photo-1597223557154-721c1cecc4b0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80',
+        text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum necessitatibus est, ratione sunt, quos quasi neque dolorem corporis deleniti illum assumenda praesentium tempore eum recusandae id magni cum similique tenetur harum! Cupiditate, architecto expedita!'
+    },
+    {
+        name: 'Jane Huston',
+        position: 'HR',
+        photo: 'https://images.unsplash.com/photo-1519362909365-f8591adb630e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80',
         text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum necessitatibus est, ratione sunt, quos quasi neque dolorem corporis deleniti illum assumenda praesentium tempore eum recusandae id magni cum similique tenetur harum! Cupiditate, architecto expedita!'
     },
 ]
@@ -33,7 +45,7 @@ function updateTestimonial() {
     testimonial.innerHTML = text
     userImage.src = photo
     username.innerHTML = name
-    func.innerHTML = role
+    role.innerHTML = position
     
     idx++
     
@@ -42,5 +54,5 @@ function updateTestimonial() {
     }
 }
 
-setInterval(updateTestimonial, 10000)
+setInterval(updateTestimonial, 2000)
 
