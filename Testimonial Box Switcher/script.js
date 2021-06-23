f2431f3dfe66e81be87952b50ccd69317251ffba
Albena Kostova <albena.kostov@gmail.com>
2021-06-24 00:39:26 +0200

    .

--- Testimonial Box Switcher/script.js
+++ Testimonial Box Switcher/script.js
@@ -6,6 +6,12 @@ const role = document.querySelector('.role')
 
 const testimonials = [
     {
+        name: 'Iva Angolff',
+        position: 'Content Writer',
+        photo: 'https://images.unsplash.com/photo-1485875437342-9b39470b3d95?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=752&q=80',
+        text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum necessitatibus est, ratione sunt, quos quasi neque dolorem corporis deleniti illum assumenda praesentium tempore eum recusandae id magni cum similique tenetur harum! Cupiditate, architecto expedita!'
+    },
+    {
         name: 'Eddie Jacson',
         position: 'Musician',
         photo: 'https://images.unsplash.com/photo-1519345182560-3f2917c472ef?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80',
@@ -30,9 +36,33 @@ const testimonials = [
         text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum necessitatibus est, ratione sunt, quos quasi neque dolorem corporis deleniti illum assumenda praesentium tempore eum recusandae id magni cum similique tenetur harum! Cupiditate, architecto expedita!'
     },
     {
+        name: 'Isack Kotuga',
+        position: 'Photographer',
+        photo: 'https://images.unsplash.com/photo-1531384698654-7f6e477ca221?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=345&q=80',
+        text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum necessitatibus est, ratione sunt, quos quasi neque dolorem corporis deleniti illum assumenda praesentium tempore eum recusandae id magni cum similique tenetur harum! Cupiditate, architecto expedita!'
+    },
+    {
+        name: 'Mark Williams',
+        position: 'Choreographer',
+        photo: 'https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=334&q=80',
+        text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum necessitatibus est, ratione sunt, quos quasi neque dolorem corporis deleniti illum assumenda praesentium tempore eum recusandae id magni cum similique tenetur harum! Cupiditate, architecto expedita!'
+    },
+    {
         name: 'Jane Huston',
         position: 'HR',
         photo: 'https://images.unsplash.com/photo-1519362909365-f8591adb630e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80',
+        text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum necessitatibus est, ratione sunt, quos quasi neque dolorem corporis deleniti illum assumenda praesentium tempore eum recusandae id magni cum similique tenetur harum! Cupiditate, architecto expedita!'
+    },    
+    {
+        name: 'Jacky Bo',
+        position: 'Creative Director',
+        photo: 'https://images.unsplash.com/photo-1579105728744-9d6b14a45389?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=330&q=80',
+        text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum necessitatibus est, ratione sunt, quos quasi neque dolorem corporis deleniti illum assumenda praesentium tempore eum recusandae id magni cum similique tenetur harum! Cupiditate, architecto expedita!'
+    },    
+    {
+        name: 'Joe Caponne',
+        position: 'Executive Director',
+        photo: 'https://images.unsplash.com/photo-1586083702768-190ae093d34d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=395&q=80',
         text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum necessitatibus est, ratione sunt, quos quasi neque dolorem corporis deleniti illum assumenda praesentium tempore eum recusandae id magni cum similique tenetur harum! Cupiditate, architecto expedita!'
     },
 ]
